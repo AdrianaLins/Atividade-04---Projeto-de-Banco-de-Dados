daaa3f3c42419558a3c2061b0548b594b42c3322
Adriana Pecorelli <adrianapecorelli.ap@gmail.com>
2021-06-01 12:11:36 -0300

    Atividade 04 - Projeto de Banco de Dados

---
+++ Atividade 04 - Projeto de Banco de Dados.sql
@@ -0,0 +1,33 @@
+DELIMITER $$
+CREATE TRIGGER tri_vendas_ai
+AFTER INSERT ON comivenda
+FOR EACH ROW
+BEGIN
+	DECLARE vtotal_itens FLOAT(10,2) DEFAULT 0; 
+    DECLARE vtotal_item FLOAT(10,2);
+    DECLARE total_item FLOAT(10,2) DEFAULT 0; 
+	DECLARE quantidade_item INT DEFAULT 0;
+    DECLARE fimloop INT DEFAULT 0;
+
+    DECLARE busca_itens CURSOR FOR
+		SELECT n_valoivenda, n_qtdeivenda
+          FROM comivenda
+		 WHERE n_numevenda = NEW.n_numevenda;
+
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET fimloop = 1;
+    
+OPEN busca_itens;
+itens : LOOP
+	IF fimloop = 1 THEN
+	LEAVE itens;
+	END IF;
+	
+	FETCH busca_itens INTO total_item, quantidade_item;
+	SET vtotal_itens = vtotal_itens + (total_item * quantidade_item);		
+END LOOP itens;     
+CLOSE busca_itens;
+
+UPDATE comvenda SET n_totavenda = vtotal_itens - (NEW.n_valoivenda * NEW.n_qtdeivenda)
+WHERE n_numevenda = NEW.n_numevenda;
+END $$
+DELIMITER ;
